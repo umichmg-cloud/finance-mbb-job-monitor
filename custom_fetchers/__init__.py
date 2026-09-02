438736718778a9589f440e772d126f51252ca776
umichmg-cloud <umichmg@gmail.com>
2026-09-02 11:24:27 +0800

    Update __init__.py

--- custom_fetchers/__init__.py
+++ custom_fetchers/__init__.py
@@ -1,4 +1,7 @@
 def fetch_custom(source):
+    """
+    Route custom sources to the correct fetcher.
+    """
 
     fetcher = source["fetcher"]
 
@@ -49,6 +52,80 @@ def fetch_custom(source):
             source
         )
 
+
+    # ========================================================
+    # TAL.NET
+    #
+    # Bank of America
+    # Evercore
+    # Rothschild & Co
+    # ========================================================
+
+    if fetcher == "talnet":
+
+        from .talnet import (
+            fetch_talnet
+        )
+
+        return fetch_talnet(
+            source
+        )
+
+
+    # ========================================================
+    # BRASSRING
+    #
+    # UBS
+    # ========================================================
+
+    if fetcher == "brassring":
+
+        from .brassring import (
+            fetch_brassring
+        )
+
+        return fetch_brassring(
+            source
+        )
+
+
+    # ========================================================
+    # YELLO
+    #
+    # Deutsche Bank
+    # ========================================================
+
+    if fetcher == "yello":
+
+        from .yello import (
+            fetch_yello
+        )
+
+        return fetch_yello(
+            source
+        )
+
+
+    # ========================================================
+    # AVATURE
+    #
+    # Macquarie
+    # ========================================================
+
+    if fetcher == "avature":
+
+        from .avature import (
+            fetch_avature
+        )
+
+        return fetch_avature(
+            source
+        )
+
+
+    # ========================================================
+    # UNKNOWN
+    # ========================================================
 
     raise ValueError(
         f"Unknown custom fetcher: "
